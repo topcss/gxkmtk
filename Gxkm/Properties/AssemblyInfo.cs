4e40d6aadc9cb8150d49bcd9cd20e686045bc558
Jack <topcss@gmail.com>
2019-07-17 09:29:53 +0800

    加入logo

--- Gxkm/Properties/AssemblyInfo.cs
+++ Gxkm/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // 有关程序集的一般信息由以下
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("Gxkm")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("全国公需科目题库")]
+[assembly: AssemblyDescription("全国公需科目题库 无限搜索版")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Gxkm")]
+[assembly: AssemblyCompany("爱脑图团队")]
+[assembly: AssemblyProduct("全国公需科目题库")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
